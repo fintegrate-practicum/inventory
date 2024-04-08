f6f3343010fc80647adfbc95176f393c1822c726
tzippykovalsky <9098537@gmail.com>
2024-04-08 14:05:48 +0300

    update redux 4

--- nest/react/src/app/actions.ts
+++ nest/react/src/app/actions.ts
@@ -1,9 +1,10 @@
-import {  PayloadAction } from "@reduxjs/toolkit"
-
-export const deleteItem = (state:any, action:PayloadAction<{ [key: string]: any }>) => {
-    state.data = state.data.filter((item: { [key: string]: any })  => item.id !== action.payload);
-  };
+import {  PayloadAction } from "@reduxjs/toolkit";
   
+
+export const deleteItem = (state: any, action: PayloadAction<string>) => {
+    state.data = state.data.filter((item: { id: string }) => item.id !== action.payload);
+};
+
   export const addItem = (state:any, action:PayloadAction) => {
     state.data = [...state.data, action.payload];
   };
