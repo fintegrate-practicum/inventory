24622ef3aa32ad652e59a35e18bc448d02717350
braindi <77191b.com>
2024-04-10 09:55:43 +0300

    extension

--- nest/src/entities/Product.ts
+++ nest/src/entities/Product.ts
@@ -5,9 +5,16 @@ import { Component } from './Component';
 @Entity()
 
 export class Product {
+    @Column({nullable:false})
+    productName:string
+
+    @Column({nullable:false})
+    productDescription:string
 
     @Column({ nullable: false })
     productComponents: Component[];
+    @Column({nullable:false})
+        componentsImages:string[]
 
     @Column({ nullable: false })
     @Min(1,{message:"price must be positive"})
