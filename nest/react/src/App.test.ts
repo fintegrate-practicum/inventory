21f12fcaef3ca8f8ab4c3c41e50edbd8a5326923
rachelosh <racheli326040@gmail.com>
2024-04-11 14:40:18 +0300

    commit3- before pr

--- nest/react/src/App.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, describe, it } from 'vitest'
-
-describe('נכון ולא נכון', () => {
-    it('true to be true', () => {
-        expect(true).toBe(true);
-    })
-
-})
--- nest/react/src/componnents/notification/Notification.tsx
+++ nest/react/src/componnents/notification/Notification.tsx
@@ -1,33 +1,20 @@
-import { FunctionComponent, useState } from 'react';
+import { FunctionComponent} from 'react';
 import Box from '@mui/material/Box';
-import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
-import { blue } from '@mui/material/colors';
+import Snackbar from '@mui/material/Snackbar';
 
-interface State extends SnackbarOrigin {
-    open: boolean;
-}
 type NotificationProps = {
     messege: string,
     vertical: 'top'|'bottom',
     horizontal: 'right'|'left'|'center',
 }
 const Notification: FunctionComponent<NotificationProps> = ({ messege, vertical, horizontal }) => {
-
-    // const [state] = useState<State>({
-    //     open: true,
-    //     vertical: 'top',
-    //     horizontal: 'center',
-    // });
-    // const { vertical, horizontal, open } = state;
-
     return (
-        <Box sx={{ width: 500 }}>
+        <Box sx={{ width: 500 }} >
             <Snackbar
                 anchorOrigin={{ vertical, horizontal }}
                 open={true}
                 message={messege}
                 key={vertical + horizontal}
-                sx={{ backgroundColor: blue }}
             />
         </Box>
     );
