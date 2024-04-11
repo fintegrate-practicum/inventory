65d44209aaf510d95ad7ea0d243819540e912993
tzippykovalsky <9098537@gmail.com>
2024-04-11 10:22:31 +0300

    add interfaces

--- nest/react/src/app/actions.ts
+++ nest/react/src/app/actions.ts
@@ -1,26 +1,31 @@
-import {  PayloadAction } from "@reduxjs/toolkit";
-  
+import { PayloadAction } from "@reduxjs/toolkit";
 
-export const deleteItem = (state: any, action: PayloadAction<string>) => {
-    state.data = state.data.filter((item: { id: string }) => item.id !== action.payload);
+
+export interface Item {
+  id: string;
+}
+
+export interface State {
+  data: Item[];
+}
+
+export const deleteItem = (state: State, action: PayloadAction<string>) => {
+  state.data = state.data.filter((item) => item.id !== action.payload);
 };
 
-  export const addItem = (state:any, action:PayloadAction) => {
-    state.data = [...state.data, action.payload];
-  };
-  
-  export const setData = (state:any, action:PayloadAction) => {
-    state.data = action.payload;
-  };
-  
+export const addItem = (state: State, action: PayloadAction<Item>) => {
+  state.data = [...state.data, action.payload];
+};
 
-  export const updateItem = (state: any, action:PayloadAction<{ [key: string]: any }>) => {
-    const updatedIndex = state.data.findIndex((item: { [key: string]: any }) => item.id === action.payload.id);
-    if (updatedIndex !== -1) {
-        const updatedData = [...state.data];
-        updatedData[updatedIndex] = action.payload; 
-        state.data = updatedData; 
-    }
+export const setData = (state: State, action: PayloadAction<Item[]>) => {
+  state.data = action.payload;
+};
 
-
+export const updateItem = (state: State, action: PayloadAction<Item>) => {
+  const updatedIndex = state.data.findIndex((item) => item.id === action.payload.id);
+  if (updatedIndex !== -1) {
+    const updatedData = [...state.data];
+    updatedData[updatedIndex] = action.payload;
+    state.data = updatedData;
+  }
 };
