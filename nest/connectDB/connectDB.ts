2b8fac53e076453a521c74fbc7d3f36e5c0fcad1
m0548548312 <y0548486067@gmail.com>
2024-04-11 12:11:27 +0300

    connectDB

---
+++ nest/connectDB/connectDB.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { ConfigService } from '@nestjs/config';
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const configService = new ConfigService();
+        await mongoose.connect(configService.get<string>('MONGO_URI'), {
+            dbName: configService.get<string>('DB_NAME'),
+            user: configService.get<string>('DB_USER'),
+        });
+        console.log('MongoDB Connected Successfully');
+    } catch (error) {
+        console.log("MongoDB Not Connected"+error);
+    }
+};
+
+export default connectDB;
