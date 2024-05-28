42f650736d39dd0eeeec390aa98cb7a8fc0681eb
רבקי מינצברג <Rivkimi>
2024-05-28 11:18:27 +0300

    interface

---
+++ nest/react/src/interfaces/IProduct.ts
@@ -0,0 +1,16 @@
+interface IProduct{
+    id: string;
+    productName: string;
+    productDescription: string;
+    componentsImages: string[];
+    packageCost: number;
+    productComponents: any[];
+    totalPrice: number;
+    adminId: string;
+    isActive: boolean;
+    isOnSale: boolean;
+    salePercentage: number;
+    stockQuantity: number;
+    bussinesId: string;
+    componentStatus: string;
+}
