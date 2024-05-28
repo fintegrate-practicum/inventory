6502131b927fca67f01427ee6a79fc830eed98ed
naama-dev <n0527192989@gmail.com>
2024-05-28 09:35:56 +0300

    changed APP

--- nest/react/src/App.tsx
+++ nest/react/src/App.tsx
@@ -1,12 +1,10 @@
-import  React,{ useState, Suspense  } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-const LazyAddProduct = React.lazy(()=>import('./components/AddProductForm'));
+
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -24,11 +22,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-     
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
-
 export default App
 
