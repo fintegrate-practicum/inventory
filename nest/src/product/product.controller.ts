9e89c78b147237254bf4ac845ba033c4b0226d5e
tzippykovalsky <9098537@gmail.com>
2024-06-30 13:53:27 +0300

    update function

--- nest/src/product/product.controller.ts
+++ nest/src/product/product.controller.ts
@@ -1,40 +1,45 @@
-import { Controller, Delete, Get, Param, Put, Post, Body, Headers } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Put, Post, Body, Headers, HttpException, HttpStatus } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Product } from './product.entity';
 
 @Controller('api/inventory/product')
 export class ProductController {
-    constructor(private readonly ProductsService: ProductService) { }
+    constructor(private readonly productsService: ProductService) { }
 
     @Get(':productId')
     getProductById(@Param('productId') productId: string) {
-        return this.ProductsService.getProductById(productId);
+        return this.productsService.getProductById(productId);
     }
 
     @Get()
     getAllProducts() {
-        return this.ProductsService.getAllProducts();
+        return this.productsService.getAllProducts();
     }
 
     @Put('sale/')
     updateProductDiscount(@Headers('x-access-token') token: string, @Body() newSalePercentage: number) {
-        return this.ProductsService.updateProductDiscount(newSalePercentage, token);
+        return this.productsService.updateProductDiscount(newSalePercentage, token);
     }
 
     @Put(':productId')
     updateProduct(@Headers('x-access-token') token: string, @Param('productId') productId: string, @Body() updatedFields: any) {
-        return this.ProductsService.updateProduct(productId, updatedFields, token);
+        return this.productsService.updateProduct(productId, updatedFields, token);
     }
 
     @Post()
     async addNewProduct(@Headers('x-access-token') token: string, @Body() newProduct: Product) {
-        await this.ProductsService.addNewProduct(newProduct, token);
-        return { message: 'Product added succefully' };
+        try {
+            await this.productsService.addNewProduct(newProduct, token);  
+            return { message: 'Product added successfully' };
+        }
+        catch(err) {
+            console.log(err);
+        }
     }
 
     @Delete(':productId')
     async softDeleteProduct(@Param('productId') productId: string) {
-        await this.ProductsService.softDeleteProduct(productId);
+        await this.productsService.softDeleteProduct(productId);
         return { message: 'Product soft deleted successfully' };
     }
 
