bd753eb5c323293587909aaf740f449bb1167080
noah-cohen <n0548548106@gmail.com>
2024-07-10 12:27:50 +0200

    check husky config

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,12 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
 	await app.listen(3000);
 	console.log('app is listening on port 3000');
 	connectDB();
-
+	const g = 5;
 }
 bootstrap();
