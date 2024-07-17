5566c03b7a4eb3619c3a1e64036f90db82d7a22a
noah-cohen <n0548548106@gmail.com>
2024-07-18 01:48:46 +0200

    update-connection-to-mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ComponentModule } from './component/component.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
 import { ProviderModule } from './provider/provider.module';
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({ envFilePath: '.env' }),
-		MongooseModule.forRoot(process.env.MONGO_URI),
-		ComponentModule, ProductModule, ProviderModule
-	],
-	controllers: [AppController],
-	providers: [AppService],
-
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    // MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: process.env.MONGODB_URI,
+      }),
+      inject: [ConfigService],
+    }),
+    ComponentModule,
+    ProductModule,
+    ProviderModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  async onModuleInit() {
+    try {
+      // הודעה כאשר התחברות למסד הנתונים מוצלחת
+      console.log('Connected 1  to MongoDB successfully!');
+    } catch (error) {
+      // הודעת שגיאה אם יש בעיה בהתחברות למסד הנתונים
+      console.error('Failed 1  to connect to MongoDB:', error);
+    }
+  }
+}
--- src/connectDB/connectDB.ts
+++ src/connectDB/connectDB.ts
@@ -2,15 +2,15 @@ import mongoose from 'mongoose';
 import { ConfigService } from '@nestjs/config';
 
 const connectDB = async () => {
-    try {
-        const configService = new ConfigService();
-        await mongoose.connect(configService.get<string>('MONGO_URI'), {
-            dbName: configService.get<string>('DB_NAME'),
-        });
-        console.log('MongoDB Connected Successfully');
-    } catch (error) {
-        console.log("MongoDB Not Connected "+error);
-    }
+  try {
+    const configService = new ConfigService();
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: process.env.DM_NAME,
+    });
+    console.log('MongoDB Connected 2  Successfully');
+  } catch (error) {
+    console.log('MongoDB Not Connected 2 ' + error);
+  }
 };
 
- export default connectDB;
+export default connectDB;
