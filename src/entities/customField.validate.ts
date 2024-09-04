d6d8d68e89eb3620b0348dc8d4c3c8ec6e7c7eb2
RivkyN <rivkyneumann1223@gmail.com>
2024-09-04 16:59:21 +0300

    Update customField.validate.ts

--- src/entities/customField.validate.ts
+++ src/entities/customField.validate.ts
@@ -1,12 +1,10 @@
 import * as Joi from '@hapi/joi';
 
-// Define the schema for options
 const optionSchema = Joi.object({
   value: Joi.string().optional(),
   label: Joi.string().optional(),
 });
 
-// Define the schema for CustomField
 export const customFieldSchema: Joi.ObjectSchema = Joi.object({
   fieldName: Joi.string().optional(),
   fieldType: Joi.string()
@@ -28,18 +26,3 @@ export const customFieldSchema: Joi.ObjectSchema = Joi.object({
   options: Joi.array().items(optionSchema).optional().default([]),
   isRequired: Joi.boolean().default(false),
 });
-
-export enum ComponentType {
-  Button = 'Button',
-  ButtonGroup = 'ButtonGroup',
-  Checkbox = 'Checkbox',
-  FloatingActionButton = 'FloatingActionButton',
-  RadioGroup = 'RadioGroup',
-  Rating = 'Rating',
-  Select = 'Select',
-  Slider = 'Slider',
-  Switch = 'Switch',
-  TextField = 'TextField',
-  MenuItem = 'MenuItem',
-  Input = 'Input',
-}
